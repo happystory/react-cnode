649d9beb8a25d940463098a9545e9ea9d5fbe06b
liuyao <670652018@qq.com>
2018-09-20 00:47:04 +0800

    ssr

--- build/webpack.server.conf.js
+++ build/webpack.server.conf.js
@@ -1,19 +1,23 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const baseWebpackConfig = require('./webpack.base.conf');
 
 module.exports = merge(baseWebpackConfig, {
   mode: 'production',
+  target: 'node',
+  entry: {
+    app: './client/server-entry.js'
+  },
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'static/js/[name].[chunkhash].js',
-    publicPath: '/'
+    filename: 'server-entry.js',
+    publicPath: '/public/',
+    libraryTarget: 'commonjs2'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../client/template.html'),
-      filename: 'index.html',
+      filename: 'server.html',
     }),
   ]
 });
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const ReactSSR = require('react-dom/server');
+const serverEntry = require('../dist/server-entry').default;
+
+const template = fs.readFileSync(path.join(__dirname, '../dist/server.html'), 'utf8');
+
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, '../dist')));
+
+app.get('/', (req, res) => {
+  const appString = ReactSSR.renderToString(serverEntry);
+  const tempStr = template.replace('<app></app>', appString);
+  res.send(tempStr);
+});
+
+app.listen(3333, () => {
+  console.log('Server is running at http://localhost:3333/');
+});
