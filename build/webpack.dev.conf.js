a219025f90f5e3e494263c9937369d6bfb4c0ff9
liuyao <670652018@qq.com>
2018-09-20 23:18:31 +0800

    meet a bug

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,16 +2,21 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 const baseWebpackConfig = require('./webpack.base.conf');
+
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = (process.env.PORT && Number(process.env.PORT)) || 8888;
 
 module.exports = merge(baseWebpackConfig, {
   mode: 'development',
   devServer: {
-    host: '0.0.0.0',
-    port: '8888',
+    host: HOST,
+    port: PORT,
     contentBase: false,
     hot: true,
     overlay: { warnings: false, errors: true },
+    quiet: true,
     historyApiFallback: {
       index: '/public/index.html'
     }
@@ -22,6 +27,13 @@ module.exports = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: path.resolve(__dirname, '../client/template.html'),
       inject: true
+    }),
+    new FriendlyErrorsPlugin({
+      compilationSuccessInfo: {
+        messages: [`Your application is running here: http://${HOST}:${PORT}`],
+      },
+      onErrors: undefined,
+      clearConsole: true,
     }),
   ]
 });
--- server/server.js
+++ server/server.js
@@ -2,19 +2,25 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const ReactSSR = require('react-dom/server');
-const serverEntry = require('../dist/server-entry').default;
 
-const template = fs.readFileSync(path.join(__dirname, '../dist/server.html'), 'utf8');
+const isDev = process.env.NODE_ENV === 'development';
 
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, '../dist')));
+if (!isDev) {
+  const serverEntry = require('../dist/server-entry').default;
+  const template = fs.readFileSync(path.join(__dirname, '../dist/server.html'), 'utf8');
+  app.use('/public', express.static(path.join(__dirname, '../dist')));
 
-app.get('/', (req, res) => {
-  const appString = ReactSSR.renderToString(serverEntry);
-  const tempStr = template.replace('<!-- app -->', appString);
-  res.send(tempStr);
-});
+  app.get('/', (req, res) => {
+    const appString = ReactSSR.renderToString(serverEntry);
+    const tempStr = template.replace('<!-- app -->', appString);
+    res.send(tempStr);
+  });
+} else {
+  const devStatic = require('./utils/dev-static');
+  devStatic(app);
+}
 
 app.listen(3333, () => {
   console.log('Server is running at http://localhost:3333/');
