3abc5284501ec5f9f8b820facd1cf86efaa803d4
liuyao <670652018@qq.com>
2019-04-04 21:30:53 +0800

    add nodemon

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const ReactSSR = require('react-dom/server');
 
 const app = express();
 const isDev = process.env.NODE_ENV === 'development';
 
+app.use(favicon(path.join(__dirname, '../favicon.ico')));
+
 if (!isDev) {
   const serverEntry = require('../dist/server-entry').default;
   const template = fs.readFileSync(
     path.join(__dirname, '../dist/index.html'),
-    'utf8'
+    'utf8',
   );
 
   app.use('/public', express.static(path.join(__dirname, '../dist')));
